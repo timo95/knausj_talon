46a614d4a205720615428594e163b3d7a2d29893
Timo Adameit <adameit@informatik.uni-leipzig.de>
2021-10-15 13:25:01 +0200

    Improve file layout

--- apps/platforms/linux/gnome-terminal/gnome-terminal.py
+++ apps/platforms/linux/gnome-terminal/gnome-terminal.py
@@ -1,45 +1,50 @@
-from talon import Context, actions
+from talon import Context, actions, Module
 
-ctx = Context()
-ctx.matches = r"""
-app.name: Gnome-terminal
+# App definition
+mod = Module()
+mod.apps.gnome_terminal = """
+os: linux
+and app.exe: gnome-terminal-server
+os: linux
+and app.name: Gnome-terminal
 """
 
+# Context matching
+ctx = Context()
+ctx.matches = r"""
+app: gnome_terminal
+"""
+
+# Set tags
+ctx.tags = ["terminal", "user.generic_unix_shell", "user.tabs", "user.git", "user.docker"]
+
+
+# --- Implement actions ---
 @ctx.action_class("user")
 class user_actions:
-    # tabs-tag functions implementations
+    # user.tabs
     def tab_jump(number):
         actions.key("alt-{}".format(number))
 
+
 @ctx.action_class("app")
 class app_actions:
-    # tabs-tag functions implementations
-    def tab_open():
-        actions.key("ctrl-shift-t")
+    # app.tabs
+    def tab_open(): actions.key("ctrl-shift-t")
+    def tab_previous(): actions.key("ctrl-pageup")
+    def tab_next(): actions.key("ctrl-pagedown")
+    def tab_close(): actions.key("ctrl-shift-w")
+    # global (overwrite linux/app.py)
+    def window_open(): actions.key('ctrl-shift-n')
 
-    def tab_previous():
-        actions.key("ctrl-pageup")
 
-    def tab_next():
-        actions.key("ctrl-pagedown")
-
-    def tab_close():
-        actions.key("ctrl-shift-w")
-
-    def window_open():
-        actions.key('ctrl-shift-n')
-
-# this overwrites the unfitting parts of linux/edit.py
+# global (overwrite linux/edit.py)
 @ctx.action_class('edit')
 class EditActions:
-    def page_down():
-        actions.key('shift-pagedown')
-    def page_up():
-        actions.key('shift-pageup')
-    def paste():
-        actions.key('ctrl-shift-v')
-    def copy():
-        actions.key('ctrl-shift-c')
+    def page_down(): actions.key('shift-pagedown')
+    def page_up(): actions.key('shift-pageup')
+    def paste(): actions.key('ctrl-shift-v')
+    def copy(): actions.key('ctrl-shift-c')
     def find(text: str):
         actions.key('ctrl-shift-f')
         if str:
@@ -47,15 +52,10 @@ class EditActions:
     def delete_line():
         actions.edit.line_start()
         actions.key('ctrl-k')
-    def extend_left():
-        pass
-    def extend_right():
-        pass
-    def extend_up():
-        pass
-    def extend_down():
-        pass
-    def extend_word_left():
-        pass
-    def extend_word_right():
-        pass
+    # afaik not possible in gnome-terminal
+    def extend_left(): pass
+    def extend_right(): pass
+    def extend_up(): pass
+    def extend_down(): pass
+    def extend_word_left(): pass
+    def extend_word_right(): pass
--- apps/platforms/linux/gnome-terminal/gnome-terminal.talon
+++
@@ -1,8 +0,0 @@
-os: linux
-app.name: Gnome-terminal
--
-tag(): terminal
-tag(): user.git
-tag(): user.docker
-tag(): user.tabs
-tag(): user.generic_unix_shell
