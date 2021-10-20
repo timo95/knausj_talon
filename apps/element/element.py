1a3056c19a9e0b98b84c59db9192e294e9d4972b
timo95 <timo95@users.noreply.github.com>
2021-10-20 07:40:46 +0200

    Improve element implementation

--- apps/element/element.py
+++ apps/element/element.py
@@ -1,21 +1,24 @@
 from talon import Context, Module, actions
 
-ctx = Context()
+# --- App definition ---
 mod = Module()
-apps = mod.apps
-apps.element = "app.name: Element"
 mod.apps.element = """
 app.name: Element
 os: windows
 and app.exe: Element.exe
 """
+
+# Context matching
+ctx = Context()
 ctx.matches = r"""
 app: element
 """
 
 
+# --- Implement actions ---
 @ctx.action_class("edit")
 class EditActions:
+    # edit
     def line_insert_down():
         actions.edit.line_end()
         actions.key("shift-enter")
@@ -23,36 +26,15 @@ class EditActions:
 
 @ctx.action_class("user")
 class UserActions:
-    def messaging_workspace_previous():
-        pass
-
-    def messaging_workspace_next():
-        pass
-
-    def messaging_open_channel_picker():
-        actions.key("ctrl-k")
-
-    def messaging_channel_previous():
-        actions.key("alt-up")
-
-    def messaging_channel_next():
-        actions.key("alt-down")
-
-    def messaging_unread_previous():
-        actions.key("alt-shift-up")
-
-    def messaging_unread_next():
-        actions.key("alt-shift-down")
-
-    def messaging_open_search():
-        actions.key("ctrl-f")
-
-    def messaging_mark_workspace_read():
-        pass
-
-    def messaging_mark_channel_read():
-        actions.key("esc")
-
-    # Files and Snippets
-    def messaging_upload_file():
-        pass
+    # user.messaging
+    def messaging_workspace_previous(): pass
+    def messaging_workspace_next(): pass
+    def messaging_open_channel_picker(): actions.key("ctrl-k")
+    def messaging_channel_previous(): actions.key("alt:down up alt:up")
+    def messaging_channel_next(): actions.key("alt:down down alt:up")
+    def messaging_unread_previous(): actions.key("alt-shift-up")
+    def messaging_unread_next(): actions.key("alt-shift-down")
+    def messaging_open_search(): actions.key("ctrl-f")
+    def messaging_mark_workspace_read(): pass
+    def messaging_mark_channel_read(): actions.key("esc")
+    def messaging_upload_file(): pass
--- apps/element/element.talon
+++ apps/element/element.talon
@@ -3,11 +3,12 @@ app: element
 tag(): user.messaging
 
 # Spaces
-workspace <number_small>: key("ctrl-{number}")
+workspace <number_small>: key("ctrl-{number_small}")
 # Messaging
 grab left: key(shift-up)
 grab right: key(shift-down)
-(insert command | commandify):
+# New format
+insert command:
     insert("``")
     key(left)
 insert code:
@@ -16,6 +17,16 @@ insert code:
     key(shift-enter)
     key(shift-enter)
     key(up)
-element (quotes | quotation): key(ctrl->)
-bold: key(ctrl-b)
-(italic | italicize): key(ctrl-i)
+insert bold:
+    insert("****")
+    key(left left)
+insert (quotation | citation):
+    key(shift-enter)
+    insert("> ")
+insert italic:
+    insert("__")
+    key(left)
+# Format selected word
+bold that: key(ctrl-b)
+(quote | cite) that: key(ctrl->)
+(italic | italicize) that: key(ctrl-i)
