16c2fad53cbc689fcb428695f2060e84c1575ec5
timo95 <timo95@users.noreply.github.com>
2021-11-06 15:13:10 +0100

    Add pages

---
+++ apps/web/amazon/amazon_pages.py
@@ -0,0 +1,19 @@
+from talon import Context, actions
+
+# Search result (query "page")
+# /s
+# /-/<lang>/s
+ctx = Context()
+ctx.matches = r"""
+app: amazon
+browser.path: /^(\/-\/\w+)?\/s$/
+"""
+ctx.tags = ["user.pages"]
+
+@ctx.action_class("user")
+class UserActions:
+    # user.pages
+    def page_current(): return int(actions.user.browser_url_query().get("page", "1"))
+    def page_jump(number: int):
+        if number > 0:
+            actions.user.browser_set_url_query("page", number)
