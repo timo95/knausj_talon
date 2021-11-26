62acfb529600398dcd2e3b6bb6f59616e5fcc294
timo95 <timo95@users.noreply.github.com>
2021-11-26 15:28:38 +0100

    Fix bug

--- apps/web/literotica/literotica.py
+++ apps/web/literotica/literotica.py
@@ -3,7 +3,7 @@ from talon import Module
 # --- App definition ---
 mod = Module()
 # not including shop and forum, since those are arguably different websites
-mod.apps.literotica = """
+mod.apps.literotica = r"""
 tag: browser
 browser.host: /^((www|tags|search|chat)\.)?literotica.com$/
 """
